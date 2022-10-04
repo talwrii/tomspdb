fa715996c257db576985669e8e44505f1ca4047b
Tal Wrii <talwrii@gmail.com>
2022-10-04 21:24:41 +0100

    deploy and tweak metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="tomspdb",
-    version=0.1,
+    version=0.2,
     author="Talwrii",
     author_email="talwrii@gmail.com",
     description="",
-    license="GPLv3",
+    license="MIT",
     keywords="debugger",
     modules=["tomspdb"],
     long_description=open("readme.md").read(),
