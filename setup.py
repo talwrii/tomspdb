91e1b535f7f4faa8e90ecfccc928f231cfbf7fe1
Tal Wrii <talwrii@gmail.com>
2022-11-04 15:51:42 +0000

    fix bug where was only installing on 3.10 I think this is to do with modules versus py_modules

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os.path
 import os
 
-HERE = os.path.dirname(__file__)
+HERE = os.path.dirname(__file__) or "."
 
 os.chdir(HERE)
 
@@ -14,14 +14,14 @@ def read(filename):
 
 setuptools.setup(
     name="tomspdb",
-    version="0.8",
+    version="0.9",
     author="Talwrii",
     author_email="talwrii@gmail.com",
     url="https://github.com/talwrii/tomspdb",
     description="",
     license="MIT",
     keywords="debugger",
-    modules=["tomspdb"],
+    py_modules=["tomspdb"],
     long_description=read(os.path.join(HERE, "readme.md")),
     long_description_content_type="text/markdown",
     entry_points={"console_scripts": ["tomspdb=tomspdb.tomspdb:main"]},
